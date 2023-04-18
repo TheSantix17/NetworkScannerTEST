a43d39b4bae51864f16ef295bea992d14bd9bb39
nicomda <nicomda@gmail.com>
2023-04-18 19:11:59 +0200

    Version con soporte para scanner via ficheros

--- scanner.py
+++ scanner.py
@@ -1,26 +1,30 @@
 import socket
 
-# Host guardará la IP
-# Ports guardará los puertos a escanear
-host = input("Introduce la IP a escanear: ")
-ports = input("Introduce los puertos a escanear (separados con coma): ")
+def leer_archivo(filename):
+    #Con el archivo abierto haz lo siguiente
+    with open(filename, "r") as file:
+        lineas=file.readlines()
+        lineaslimpias=[]
+        for linea in lineas:
+            lineaslimpias.append(linea.rstrip())
+        return lineaslimpias
+        
+hosts = leer_archivo('ips.txt')
+print(hosts)
+ports = leer_archivo('puertos_comunes.txt')
 
-#Esto va a separar los puertos que hemos leido por consola
-ports = ports.split(",")
-
-#Nos dice la cantidad de puertos guardados en la array ports
-print(f"Se escanearán {len(ports)} puertos para la IP: {host}")
-
-# Para cada puerto que haya en puertos
-for puerto in ports:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(1)
-    result= sock.connect_ex((host, int(puerto)))
-    if result == 0:
-        print(f"El puerto {puerto} está abierto")
-    else:
-        print(f"El puerto {puerto} está cerrado")
-    sock.close()
+# Para cada host que hay en hosts
+for host in hosts:
+    # Para cada puerto que haya en puertos
+    for puerto in ports:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        result= sock.connect_ex((host, int(puerto)))
+        if result == 0:
+            print(f"El puerto {puerto} está abierto en el host {host}")
+        else:
+            print(f"El puerto {puerto} está cerrado en el host {host}")
+        sock.close()
 
     
     
